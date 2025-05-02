6486bbe4cfd26a10ac4e75bfec907cb46ddbfd5e
Exvigilare Gemini <dubois.adelin@gmail.com>
2025-05-02 19:36:25 +0200

    Styling : contact + condition mobile responsive

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,8 +1,9 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import style from "../style/components/leaflet.module.scss";
 export default function Map() {
   return (
-    <MapContainer center={[50.7052, 4.4015]} zoom={13} scrollWheelZoom={true} style={{ height: '25vw', width: '30vw' }}>
+    <MapContainer center={[50.7052, 4.4015]} zoom={20} scrollWheelZoom={true} className={style.mapContainer}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
