d3a3ecc938dd10247c5d5d6e4091ff502c4474a7
Exvigilare Gemini <dubois.adelin@gmail.com>
2025-05-09 18:49:11 +0200

    Style : general adjustment for good responsiveness

--- src/props/infoCardProps.js
+++ src/props/infoCardProps.js
@@ -1,23 +1,23 @@
 export const infoCardProps = [
   {
     index: "1",
-    link: "/assets/img/jacuzzi.jpeg", // ✅ chemin public correct
+    link: "/assets/img/jacuzzi.jpeg", 
     alt: "Jacuzzi",
     title: "Lorem ipsum.",
-    text: "...",
+    text: "Lorem ipsum dolor sit amet consectetur. Ipsum risus eget bibendum tempor faucibus rhoncus ornare varius tincidunt. Pellentesque bibendum tortor sed blandit vel tellus. ",
   },
   {
     index: "2",
-    link: "/assets/img/salon.jpeg", // ✅
+    link: "/assets/img/salon.jpeg", 
     alt: "Salon",
     title: "Lorem ipsum.",
-    text: "...",
+    text: "Lorem ipsum dolor sit amet consectetur. Ipsum risus eget bibendum tempor faucibus rhoncus ornare varius tincidunt. Pellentesque bibendum tortor sed blandit vel tellus. ",
   },
   {
     index: "3",
-    link: "/assets/img/chambre.jpg", // ✅
+    link: "/assets/img/chambre.jpg", 
     alt: "Chambre",
     title: "Lorem ipsum.",
-    text: "...",
+    text: "Lorem ipsum dolor sit amet consectetur. Ipsum risus eget bibendum tempor faucibus rhoncus ornare varius tincidunt. Pellentesque bibendum tortor sed blandit vel tellus. ",
   },
 ];
