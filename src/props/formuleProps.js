f897fd66544fa6d660712f40b843b57eabec96a2
Exvigilare Gemini <dubois.adelin@gmail.com>
2025-10-15 19:48:46 +0200

    Style : changing colors for better visual aspect | Feat : adding CGV page

--- src/props/formuleProps.js
+++ src/props/formuleProps.js
@@ -7,7 +7,7 @@ export const formuleProps = [
       { text: "- 4h : 210€" },
       { text: "au delà, 20€ par heure supplémentaire" },
     ],
-    link: "/assets/img/salon.jpeg",
+    link: "/assets/img/tarifparheure_resultat.webp",
   },
     {
     textItems: [
@@ -19,7 +19,7 @@ export const formuleProps = [
       { text: "Dimanche à jeudi : 329€" },
       { text: "Vendredi à samedi : 349€" },
     ],
-    link: "/assets/img/salon.jpeg",
+    link: "/assets/img/tarifnuit_resultat.webp",
   },
     {
     textItems: [
@@ -29,6 +29,6 @@ export const formuleProps = [
       { text: "Chips 4€" },
       { text: "Raffaelo 6€" },
     ],
-    link: "/assets/img/salon.jpeg",
+    link: "/assets/img/tarifboisson_resultat.webp",
   },
 ];
--- src/props/infoCardProps.js
+++ src/props/infoCardProps.js
@@ -1,22 +1,22 @@
 export const infoCardProps = [
   {
     index: "1",
-    link: "/assets/img/jacuzzi.jpeg", 
-    alt: "Jacuzzi",
+    link: "/assets/img/nuitees_resultat.webp", 
+    alt: "Nuitées",
     title: "Nuitées",
     text: "Les nuitées vous offrent un accès exclusif à l'ensemble de l'établissement et à toutes ses installations. Du jacuzzi au sauna infrarouge, de l'espace de luminothérapie au salon, en passant par la chambre avec lit rond, la table de massage, la cuisine et les deux salles de bain.",
   },
   {
     index: "2",
-    link: "/assets/img/salon.jpeg", 
-    alt: "Salon",
+    link: "/assets/img/alheure_resultat.webp", 
+    alt: "À l'heure",
     title: "À l'heure",
     text: "Pour les réservations à l'heure, l'accès comprend le sauna, le jacuzzi, le salon ainsi qu'une salle de bain équipée de douches. L'accès à la chambre avec lit rond, à la cuisine, à la table de massage et à la seconde sale de bain est possible en option, moyennant un supplément de 40€.",
   },
   {
     index: "3",
-    link: "/assets/img/chambre.jpg", 
-    alt: "Chambre",
+    link: "/assets/img/prestation_resultat.webp", 
+    alt: "Prestations",
     title: "Prestations",
     text: "Des boissons telles que des cocktails individuels, bouteilles de champagne et une sélection de softs, ainsi que de petites gourmandises comme des chips ou des Raffaello sont à disposition tout au long du séjour. Ces options, payantes, peuvent être consommées librement selon les envies.",
   },
